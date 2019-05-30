9f2c06fd1da2796163a37425d392a47f516b3cfa
martinc <martin.castro@incluit.com>
2019-05-30 14:22:56 -0300

    test2

--- lambdas/helloWorld.py
+++ lambdas/helloWorld.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello  MC pipeline')
+        'body': json.dumps('Hello  MC pipeline now')
     }
